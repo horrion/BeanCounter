39f8bdf5f60eb8a1bdbffeda5311498572f67155
Robert Horrion <horrion@me.com>
2019-10-08 23:28:39 +0200

    Initial Commit

---
+++ BeanCounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeanCounter
+//
+//  Created by Robert Horrion on 10/8/19.
+//  Copyright © 2019 Robert Horrion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
