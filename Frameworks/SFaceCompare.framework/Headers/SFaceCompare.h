91da9ef9f143fcc5d8119a662d8a77183bbf820b
Unknown <horrion@me.com>
2020-08-09 00:15:58 +0200

    Replaced CocoaPods SFaceCompare with custom build for better recognition

---
+++ Frameworks/SFaceCompare.framework/Headers/SFaceCompare.h
@@ -0,0 +1,20 @@
+//
+//  SFaceCompare.h
+//  SFaceCompare
+//
+//  Created by Robert Horrion on 8/1/20.
+//  Copyright © 2020 Robert Horrion. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+//! Project version number for SFaceCompare.
+FOUNDATION_EXPORT double SFaceCompareVersionNumber;
+
+//! Project version string for SFaceCompare.
+FOUNDATION_EXPORT const unsigned char SFaceCompareVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SFaceCompare/PublicHeader.h>
+
+
--- Podfile
+++ Podfile
@@ -1,5 +1,5 @@
 # Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+# platform :ios, '13.0'
 
 target 'BeanCounter' do
   # Comment the next line if you don't want to use dynamic frameworks
@@ -9,7 +9,6 @@ target 'BeanCounter' do
 
   pod 'SVPinView', '~> 1.0'
   pod 'KeychainSwift'
-  pod 'SFaceCompare'
 
   target 'BeanCounterTests' do
     inherit! :search_paths
